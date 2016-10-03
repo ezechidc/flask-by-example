60ef5943dec15a193dd3a41e2d7d148c9710691b
dennis <ezechidc@yahoo.com>
2016-10-03 18:29:57 +0100

    heroku

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost/wordcount_dev"
 
 class ProductionConfig(Config):
     DEBUG = False
